6987ae8e9f000c1119156bafc91c2104d048f0f1
lee <lee@gmail>
2015-03-01 16:01:51 +0800

    add: Insert function

---
+++ Refactor/Refactor/InsertItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Refactor
+{
+    public class InsertItem
+    {
+        public string Text { get; set; }
+
+        public TextPosition Postion { get; set; }
+    }
+}
---
+++ Refactor/Refactor/RefactorUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Refactor
+{
+    public class RefactorUtility
+    {
+        public string BulkInsert(string orginText, InsertItem[] insertItem, string outRowDelimiter)
+        {
+
+        }
+    }
+}
