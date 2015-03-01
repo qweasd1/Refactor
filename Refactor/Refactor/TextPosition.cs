6138f4b733d1ca051339d2a596d80c82f3ca5109
lee <lee@gmail>
2015-03-01 16:01:34 +0800

    init: Domain Model

---
+++ Refactor/Refactor/TextPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Refactor
+{
+    public class TextPosition
+    {
+        public TextPosition(int lineNumber, int columnNumber)
+        {
+            LineNumber = lineNumber;
+            ColumnNumber = columnNumber;
+        }
+        public int LineNumber { get; private set; }
+        public int ColumnNumber { get; private set; }
+    }
+}
---
+++ Refactor/Refactor/TextSection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Refactor
+{
+    public class TextSection
+    {
+        public TextPosition StartPostion { get; private set; }
+        public TextPosition EndPosition  { get; private set; }
+    }
+}
