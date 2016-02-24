4c1a1e4817d3a75418795c1df102327c53c5c9b6
qweasd1 <qweasd1@users.noreply.github.com>
2016-02-24 14:51:54 +0800

    Update test_form.py

--- MySQL/mforms/common/test_form.py
+++ MySQL/mforms/common/test_form.py
@@ -1,22 +1,27 @@
 import mforms
+tab = mforms.newTabView(mforms.TabViewTabless)
+
+#use t to reference the control you create
 
 class test(mforms.Form):
     def __init__(self):
+        global t
         mforms.Form.__init__(self,None)
         self.set_size(800,400)
         box = mforms.newBox(False)
         
         #### put your code here
-        t = mforms.newCheckBox()
-        t.set_text("select me")
-        t.set_active(True)
-        #t.set_size(600,400)
+        t = mforms.new<newControl>()
         
+       
         ####
         box.add(t,False,True)
         
         self.set_content(box)
         self.t = t
+
+    def cb(self,x):
+        print(x)
     def run(self):
         self.apply = mforms.newButton()
         self.cancel = mforms.newButton()
@@ -24,5 +29,5 @@ class test(mforms.Form):
         self.run_modal(self.apply, self.cancel)
 
 
-t = test()
-t.run()
+tm = test()
+tm.run()
